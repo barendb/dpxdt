977c42fa427e14306c92e49c10f0752dc75e2bb6
Barend Bootha <barend.bootha@pushpay.com>
2014-09-19 15:24:44 +1200

    needed to add api_keys to ./run_combined.sh as we've now enabled auth also needed make sure that that api_key is a superuser

--- run_combined.sh
+++ run_combined.sh
@@ -7,12 +7,14 @@ source common.sh
     --phantomjs_binary=$PHANTOMJS_BINARY \
     --phantomjs_script=$CAPTURE_SCRIPT \
     --phantomjs_timeout=20 \
-    --release_server_prefix=http://localhost:8080/api \
-    --queue_server_prefix=http://localhost:8080/api/work_queue \
+    --release_server_prefix=http://0.0.0.0:8080/api \
+    --queue_server_prefix=http://0.0.0.0:8080/api/work_queue \
     --queue_idle_poll_seconds=10 \
     --queue_busy_poll_seconds=10 \
     --pdiff_timeout=20 \
     --reload_code \
     --port=8080 \
     --verbose \
+    --release_client_id=yfnje6vikkazeeymtscey3gfz6uqzrrt \
+    --release_client_secret=hMGp4UWzVkFloy5tpfDxcgU4rbk \
     $@
