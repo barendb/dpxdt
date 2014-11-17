8b9a322e9552001ef8b338e234bd0cbe35db933b
Barend Bootha <barend.bootha@pushpay.com>
2014-11-18 08:59:14 +1300

    add dpxdt-server.conf this file is used by upstart to start / respawn dpxdt on the linux box be sure to add this in /etc/init when doing a fresh deployment

--- run_server.sh
+++ run_server.sh
@@ -4,7 +4,11 @@ source common.sh
 
 ./dpxdt/runserver.py \
     --reload_code \
-    --port=5000 \
+    --port=8080 \
     --verbose \
     --ignore_auth \
+    --release_server_prefix=http://0.0.0.0:8080/api \
+    --queue_server_prefix=http://0.0.0.0:8080/api/work_queue \
+    --release_client_id=yfnje6vikkazeeymtscey3gfz6uqzrrt \
+    --release_client_secret=hMGp4UWzVkFloy5tpfDxcgU4rbk \
     $@
--- run_worker.sh
+++ run_worker.sh
@@ -8,4 +8,9 @@ source common.sh
     --release_server_prefix=$RELEASE_SERVER_PREFIX \
     --queue_server_prefix=$QUEUE_SERVER_PREFIX \
     --verbose \
+    --pdiff_timeout=50 \
+    --release_client_id=yfnje6vikkazeeymtscey3gfz6uqzrrt \
+    --release_client_secret=hMGp4UWzVkFloy5tpfDxcgU4rbk \
+    --release_server_prefix=http://0.0.0.0:8080/api \
+    --queue_server_prefix=http://0.0.0.0:8080/api/work_queue \
     $@
